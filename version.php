d39bdeab2c885d4cde3bc27e18539d4b7e32d9bf
root <iseki@rsch.tuis.ac.jp>
2023-12-28 21:19:31 +0900

    v1.3.1

--- version.php
+++ version.php
@@ -7,7 +7,9 @@ $plugin->requires  = 2013051400;	   		// Requires Moodle 2.5
 $plugin->component = 'mod_homeroom';
 $plugin->maturity  = MATURITY_STABLE;
 
-$plugin->version   = 2016031300;	// デフォルト動作の変更
-$plugin->release   = '1.3.0';
+$plugin->release   = '1.3.1';
+
+$plugin->version   = 2023122800;
+//$plugin->version = 2016031300;	// デフォルト動作の変更
 
 
